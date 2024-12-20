027a52f9ccdaf6903d6f0a079b9bd10de3c89162
Felix Idén <ouc98oec@students.codeinstitute.net>
2024-12-20 08:22:33 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,9 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QSJOyDYzicqtyurN9K8RX8LJVBqMKq9eUIlMmeNs6yrRcn194RfepAIUYH4hLmENi1a0NXvXoXKQIbqJGnafbJR00nETLTe4Q',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
+    
