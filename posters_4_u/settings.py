1ac2ec036a2bc4efac86be9505e19844bf742569
Felix Idén <ouc98oec@students.codeinstitute.net>
2024-12-29 14:36:56 +0000

    Created new static folder

--- posters_4_u/settings.py
+++ posters_4_u/settings.py
@@ -179,7 +179,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
