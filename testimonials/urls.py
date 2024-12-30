0194fd4fd9412d402c8200b5918108ad88f1c86e
Felix Idén <ouc98oec@students.codeinstitute.net>
2024-12-30 21:23:16 +0000

    debugging urls

--- testimonials/urls.py
+++ testimonials/urls.py
@@ -3,7 +3,7 @@ from .import views
 
 
 urlpatterns = [
-    path('testimonials/', views.testimonials_view, name='testimonials'),
+    path('testimonials', views.testimonials_view, name='testimonials'),
     path('testimonials/add/',views.add_testimonials_view, name='add_testimonials'),
    
 ]
